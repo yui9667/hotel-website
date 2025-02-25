f6551604022b91dfea629aab54174be6a87c2a4c
Yui Jensen <kyanyui67@gmail.com>
2025-02-25 23:27:02 +0100

    Fixed an error

--- server/client/src/config.js
+++ server/client/src/config.js
@@ -1,2 +1,2 @@
-const BACKEND_URL = import.meta.env.VITE.BACKEND_URL || 'http://localhost:3002';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3002';
 export default BACKEND_URL;
