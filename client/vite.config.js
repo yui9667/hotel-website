af2823ddbd7bffc53ff5302a92d412028cde1fc6
Yui Jensen <kyanyui67@gmail.com>
2025-03-14 11:38:26 +0100

    delete proxy and added new folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://hotel-website-1-r5kh.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
